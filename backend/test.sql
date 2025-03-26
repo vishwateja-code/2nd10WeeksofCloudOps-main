d4274ce739ea1ef9c3a641d5b031d10d8741d33d
vishwateja-code <vishwatejathallapalli@gmail.com>
2025-03-26 21:50:58 +0530

    Update test.sql

--- backend/test.sql
+++ backend/test.sql
@@ -29,6 +29,7 @@ CREATE TABLE IF NOT EXISTS `books` (
 INSERT INTO `books` (`id`, `title`, `desc`, `price`, `cover`) VALUES
 (1, 'MultiCloud', 'this is mutlicloud with devops cource by veera sir nareshit ', 2343.2, 'https://docs.multy.dev/assets/images/multi-cloud-314609adeec670988dff0882a93fdcb0.png'),
 (2, 'DevOps', 'if you understand the devops it is very easy', 2342.3, 'https://media.licdn.com/dms/image/v2/D5612AQHsA9dJVtZRdw/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1730826289071?e=2147483647&v=beta&t=JGr0gJH6RE9b1Dk7nHAYF14Kmv96Gm519UOy_rSa3xE');
+(3, 'graphana', 'coming days good clarity',  500, 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FGrafana&psig=AOvVaw2mvJyVsp4tYc4IZHwG5GG0&ust=1743092234542000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMipnuOSqIwDFQAAAAAdAAAAABAE
 
 -- Add primary key for the books table
 ALTER TABLE `books`
