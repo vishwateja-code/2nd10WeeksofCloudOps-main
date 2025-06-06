6cf10b643e5f724f7d38ab15da2373a409bf4237
vishwateja-code <vishwatejathallapalli@gmail.com>
2025-06-06 16:34:15 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,6 @@
 // // const API_BASE_URL = "http://localhost:8800";
-const API_BASE_URL = "http://www.vishwateja.co.in";
+const API_BASE_URL = "http://
+a82a25fef7a0e454386211e92501beae-372157723.us-east-1.elb.amazonaws.com";
 export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 // export default API_BASE_URL;
